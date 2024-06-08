0020906a0247f3f534e5d88425c2741dd5ca6ebf
Btampolski <brunatampolski@gmail.com>
2024-06-08 10:20:31 +0900

    html plugin camera

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,21 @@
 // This is a JavaScript file
+$(document).on("click", "#camera", function() {
+    navigator.camera.getPicture(onSuccess, onFail,  {
+    quality: 50, destinationType: Camera.DestinationType.FILE_URI,
 
+     correctOrientation: true,
+    savetoPhotoAlbum: true
+
+    });
+
+    correctOrientation: true;
+    savetoPhotoAlbum: true;
+
+    function onSuccess(imageURI) {
+     var image = document.getElementById('myImagem');
+     image.src =imageURI;  }
+
+     function onFail(message){
+         alert('Failed because: ' +message);
+}
+    });
